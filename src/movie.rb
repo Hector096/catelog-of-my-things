f55db185f3fa695ead389d10ec750a34d4dcc57b
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-11 13:42:22 +0000

    Add movie class

---
+++ src/movie.rb
@@ -0,0 +1,11 @@
+class Movie
+    def initialize(id: nil,genre:,author:,source:,:label:,publish_date:,archived:,silet:)
+        super(id: id,genre: genre,author: author,source: source,label: label,publish_date:publish_date,archived:archived)
+        @silet = silet
+    end
+
+    private
+
+    def can_be_archived?
+    end
+end
