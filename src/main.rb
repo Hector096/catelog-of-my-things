dbd55f1e4d19aa5279954ef1ddbdaf949cc5d683
Leandro-Barretoo <officialdevmi@gmail.com>
2022-01-13 12:38:14 +0000

    Add main.rb as application entry point

--- src/main.rb
+++ src/main.rb
@@ -2,16 +2,63 @@ require_relative 'modules/create_book'
 require_relative 'label'
 
 class Main
-include CreateBook
+    include CreateBook
+    attr_accessor :labels
 
-attr_accessor :labels
-
-def initialize ()
-    @books = []
+    def initialize
+        @books = []
     @labels = []
+    end
+
+    def options
+        available = {
+            '1' => 'List all books',
+            '2' => 'List all music albums',
+            '3' => 'List all movies',
+            '4' => 'List all games',
+            '5' => 'List all genres',
+            '6' => 'List all labels',
+            '7' => 'List all author',
+            '8' => 'List all sources',
+            '9' => 'Add a book',
+            '10' => 'Add a music album',
+            '11' => 'Add a movie',
+            '12' => 'Add a game',
+            '13' => 'Exit'
+        }
+        puts 'Please choose an option by enterin a number:'
+        available.each { |key, value| puts "#{key} - #{value}" }
+    end
+
+    def handle_number
+        choice = gets.chomp
+        case choice
+        when '4'
+            @handler.list_games
+        when '7'
+            @handler.list_authors
+        when '12'
+            @handler.create_game
+        when '13'
+            @active = false
+            puts 'Thank you for using  this app!'
+        end
+    end
+
+    def run
+        puts 'welcome'
+        puts ''
+        while @active
+            options
+            handle_number
+        end
+    end
 end
 
-
-
+def app
+    application = Main.new
+    application.run
 end
+
+app
 
